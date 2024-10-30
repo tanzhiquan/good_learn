58900433de255a91b6151932ad665644bb33f0cf
ZhiquanTan <33016464+tanzhiquan@users.noreply.github.com>
2024-10-30 18:32:12 +0800

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
-import 'package:flutter/material.dart';
-import 'package:helloworld/screens/login.dart';
+import 'package:flutter/material.dart'; // 导入Flutter的Material Design组件库
+import 'package:shop/screens/login.dart'; // 导入自定义的登录页面组件
 
-void main() => runApp(MyApp());
+void main() => runApp(MyApp()); // 程序入口，运行MyApp应用
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget { // 定义一个无状态组件MyApp
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'app Demo',
-      home: LoginPage(),
-      debugShowCheckedModeBanner: false, // 添加这一行来隐藏debug标签
+  Widget build(BuildContext context) { // 构建组件的方法
+    return MaterialApp( // 返回一个MaterialApp组件
+      title: 'shop Demo', // 设置应用标题
+      home: LoginPage(), // 设置应用的首页为LoginPage
+      debugShowCheckedModeBanner: false,  // 禁用debug模式下的黄色横条
     );
   }
 }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -6,10 +6,8 @@ class LoginPage extends StatelessWidget {
   final TextEditingController passwordController = TextEditingController();
 
   void _login(BuildContext context) {
-    // 登录逻辑 需要补充密码校验
     var username = "admin";
     var password = "123456";
-    // 初始化账号密码
     if (usernameController.text == username && passwordController.text == password) {
       Navigator.push(
         context,
@@ -25,109 +23,46 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text('登录'),
-        backgroundColor: Colors.blue,
+        title: const Text('登录', style: TextStyle(color: Colors.black)),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.black),
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/bg.jpg"),
-            fit: BoxFit.cover,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              TextField(
+                controller: usernameController,
+                decoration: InputDecoration(
+                  labelText: '用户名',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.person, color: Colors.grey),
+                ),
+              ),
+              SizedBox(height: 16),
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: '密码',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock, color: Colors.grey),
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => _login(context),
+                child: const Text('登录'),
+              ),
+            ],
           ),
         ),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Flexible(
-                  child: TextField(
-                    controller: usernameController,
-                    decoration: InputDecoration(
-                      labelText: '请输入用户名',
-                      border: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(width: 1, color: Colors.grey),
-                      ),
-                    ),
-                  ),
-                  flex: 1,
-                ),
-                Flexible(
-                  child: TextField(
-                    controller: passwordController,
-                    decoration: InputDecoration(
-                      labelText: '请输入密码',
-                      border: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(width: 1, color: Colors.grey),
-                      ),
-                    ),
-                    obscureText: true,
-                  ),
-                  flex: 1,
-                ),
-                const SizedBox(height: 20),
-                Flexible(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      ElevatedButton(
-                        onPressed: () => _login(context),
-                        child: const Text('登录'),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                          foregroundColor: Colors.white,
-                        ),
-                      ),
-                      ElevatedButton(
-                        onPressed: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => RegisterPage()),
-                        ),
-                        child: const Text('注册'),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                          foregroundColor: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                  flex: 2,
-                ),
-                const SizedBox(height: 20),
-                Flexible(
-                  child: TextButton(
-                    onPressed: () {},
-                    child: const Text('忘记密码？'),
-                    style: TextButton.styleFrom(
-                      foregroundColor: Colors.blue,
-                      backgroundColor: Colors.transparent,
-                    ),
-                  ),
-                  flex: 1,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class RegisterPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('注册')),
-      body: const Center(
-        child: Text('注册页面'),
       ),
     );
   }
