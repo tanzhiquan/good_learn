265838f0fd27e895b296a6af8e31bbc9811fab98
ZhiquanTan <33016464+tanzhiquan@users.noreply.github.com>
2024-10-30 16:24:40 +0800

    --

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'E-commerce Demo',
+      title: 'app Demo',
       home: LoginPage(),
       debugShowCheckedModeBanner: false, // 添加这一行来隐藏debug标签
     );
